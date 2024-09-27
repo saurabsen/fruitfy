a5876fc90611deafa9279f82cfae7c22702abf80
saurab sen <saurav540@yahoo.com>
2024-09-27 21:04:08 +0530

    UI enhancement for CORS

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -40,12 +40,42 @@ const Dashboard = () => {
 
   if (error)
     return (
-      <div>
-        If data not visible please visit. Enable CORS by clicking down below{" "}
-        <Link to="https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit/all">
-          Click me
-        </Link>
-      </div>
+      <section className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Enable CORS!
+          </h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+            https://www.fruityvice.com has CORS disabled, which is blocking the
+            data to be loaded in the UI. Please click on the below link to
+            enable CORS and refresh the page.
+          </p>
+          <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a
+              href={
+                "https://cors-anywhere.herokuapp.com/https://www.fruityvice.com/api/fruit/all"
+              }
+              target="_blank"
+              rel="noreferrer"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            >
+              ENABLE CORS
+              <svg
+                className="ml-2 -mr-1 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
     );
   return (
     <div>
